4753fff8ca3cf79d0086611a77b081eb95767b1d
ubuntu <ubuntu@localhost.localdomain>
2022-04-02 21:22:26 +0800

    第三次提交，完成了日志和定时器部分

---
+++ code/timer/heaptimer.cpp
@@ -0,0 +1,145 @@
+/*
+ *计时器类方法实现
+ *2022-3-31
+*/
+
+#include "heaptimer.h"
+
+
+//向上调整
+void HeapTimer::siftup_(size_t i){
+    assert(i >= 0 && i < heap_.size());
+    size_t j = (i - 1) / 2;
+    while(j >= 0){
+        if(heap_[j] < heap_[i]){
+            break;
+        }
+        SwapNode_(i, j);
+        i = j;
+        j = (i - 1) / 2;
+    }
+}
+
+//交换节点数据
+void HeapTimer::SwapNode_(size_t i, size_t j){
+    assert(i >= 0 && i < heap_.size());
+    assert(j >= 0 && j < heap_.size());
+    std::swap(heap_[i], heap_[j]);
+    ref_[heap_[i].id] = i;
+    ref_[heap_[j].id] = j;
+}
+
+//向下调整
+bool HeapTimer::siftdown_(size_t index, size_t n){
+    assert(index >= 0 && index < heap_.size());
+    assert(n >= 0 && n <= heap_.size());
+    int i = index;
+    int j = (i * 2) + 1;
+    while(j < n){
+        if(j + 1 < n && heap_[j + 1] < heap_[j]){
+            ++j;
+        }
+        if(heap_[i] < heap_[j]){
+            break;
+        }
+        SwapNode_(i, j);
+        i = j;
+        j = i * 2 + 1;
+    }
+}
+
+//堆里没有则将新增节点，有则调整堆
+void HeapTimer::add(int id, int timeOut, const TimeoutCallBack& cb){
+    assert(id >= 0);
+    size_t i;
+    if(ref_.count(id) == 0){
+        i = heap_.size();
+        ref_[id] = i;
+        //堆尾插入，调整堆
+        heap_.push_back({id, Clock::now() + MS(timeOut), cb});
+        siftup_(i);
+    }
+    else{
+        //已有节点，调整堆
+        i = ref_[id];
+        heap_[i].expires = Clock::now() + MS(timeOut);
+        heap_[i].cb = cb;
+        if(!siftdown_(i, heap_.size())){
+            siftup_(i);
+        }
+    }
+}
+
+//删除指定节点，并触发回调函数
+void HeapTimer::doWork(int id){
+    if(heap_.empty() || ref_.count(id) == 0){
+        return;
+    }
+    size_t i = ref_[id];
+    TimerNode node = heap_[i];
+    node.cb();
+    del_(i);
+}
+
+//删除指定节点
+void HeapTimer::del_(size_t index){
+    assert(heap_.empty() && index >= 0 && index < heap_.size());
+    //将要删除的节点换到尾部，然后调整堆
+    size_t i = index;
+    size_t n = heap_.size();
+    if(i < n){
+        if(!siftdown_(i, n)){
+            siftup_(i);
+        }
+    }
+
+    //删除尾部节点
+    ref_.erase(heap_.back().id);
+    heap_.pop_back();
+}
+
+//调整指定id的节点
+void HeapTimer::adjust(int id, int timeout){
+    assert(!heap_.empty() || ref_.count(id) > 0);
+    heap_[ref_[id]].expires = Clock::now() + MS(timeout);
+    siftdown_(ref_[id], heap_.size());
+}
+
+//清除超时节点
+void HeapTimer::tick(){
+    if(heap_.empty()){
+        return;
+    }
+
+    while(!heap_.empty()){
+        TimerNode node = heap_.front();
+        if(std::chrono::duration_cast<MS>(node.expires - Clock::now()).count() > 0) { 
+            break; 
+        }
+        node.cb();
+        pop();
+    }
+}
+
+void HeapTimer::pop(){
+    assert(!heap_.empty());
+    del_(0);
+}
+
+void HeapTimer::clear(){
+    ref_.clear();
+    heap_.clear();
+}
+
+//清楚已经超时的节点后获取堆顶还有多少时间超时
+int HeapTimer::GetNextTick(){
+    tick();
+    size_t res = -1;
+    if(!heap_.empty()){
+        res = std::chrono::duration_cast<MS>(heap_.front().expires - Clock::now()).count();
+        if(res < 0){
+            res = 0;
+        }
+    }
+    return res;
+}
---
+++ code/timer/heaptimer.h
@@ -0,0 +1,65 @@
+/*
+ * @计时器类
+ * @2022-3-28
+ */ 
+#ifndef HEAP_TIMER_H
+#define HEAP_TIMER_H
+
+#include <queue>
+#include <unordered_map>
+#include <time.h>
+#include <algorithm>
+#include <arpa/inet.h> 
+#include <functional> 
+#include <assert.h> 
+#include <chrono>
+#include "../log/log.h"
+
+typedef std::function<void()> TimeoutCallBack;
+typedef std::chrono::high_resolution_clock Clock;
+typedef std::chrono::milliseconds MS;
+typedef Clock::time_point TimeStamp;
+
+struct TimerNode {
+    int id;
+    TimeStamp expires;
+    TimeoutCallBack cb;
+    bool operator<(const TimerNode& t) {
+        return expires < t.expires;
+    }
+};
+class HeapTimer {
+public:
+    HeapTimer() { heap_.reserve(64); }
+
+    ~HeapTimer() { clear(); }
+    
+    void adjust(int id, int newExpires);
+
+    void add(int id, int timeOut, const TimeoutCallBack& cb);           //新增节点
+
+    void doWork(int id);
+
+    void clear();
+
+    void tick();
+
+    void pop();
+
+    int GetNextTick();
+
+private:
+    void del_(size_t i);
+    
+    void siftup_(size_t i);                         //向上调整
+
+    bool siftdown_(size_t index, size_t n);         //向下调整
+
+    void SwapNode_(size_t i, size_t j);             //交换节点
+
+    std::vector<TimerNode> heap_;
+
+    std::unordered_map<int, size_t> ref_;
+};
+
+#endif //HEAP_TIMER_H
